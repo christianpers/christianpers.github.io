415c03e3c99854e8e68e901256f26f0631ae7a55
Christian <christianperss@gmail.com>
2019-12-22 21:37:27 +0100

    init commit

---
+++ precache-manifest.2c0980ff697df1a61dfdac12a959c8dd.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3574a93719a8f936e9632f32f9fce7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2196041c7e943b4a02d0",
+    "url": "/static/css/main.519e37e6.chunk.css"
+  },
+  {
+    "revision": "5d12e0ff77eda02bb519",
+    "url": "/static/js/2.e7c0c19e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.e7c0c19e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2196041c7e943b4a02d0",
+    "url": "/static/js/main.f9c44177.chunk.js"
+  },
+  {
+    "revision": "1b4a42b70497f7276539",
+    "url": "/static/js/runtime-main.604d0543.js"
+  },
+  {
+    "revision": "aff4e59d46867831fdbe8d2e462784ac",
+    "url": "/static/media/MillerBanner_Light.aff4e59d.otf"
+  },
+  {
+    "revision": "e5dea59bf21a7853c40bf744d5a9760a",
+    "url": "/static/media/arrow.e5dea59b.svg"
+  },
+  {
+    "revision": "b29eb6b8b11d70d3b22d62ea15f6fcdb",
+    "url": "/static/media/clock.b29eb6b8.svg"
+  },
+  {
+    "revision": "407c5db31d52f3b9b69060818779816f",
+    "url": "/static/media/empty-star.407c5db3.svg"
+  },
+  {
+    "revision": "4c9473f8ebdbabc6a901acce29d037c7",
+    "url": "/static/media/filter.4c9473f8.svg"
+  },
+  {
+    "revision": "2ffa8a52471ba3c3926b0c91a5bdda90",
+    "url": "/static/media/globe.2ffa8a52.svg"
+  },
+  {
+    "revision": "7c6c893deeb5f9c8fe02487a8952ac19",
+    "url": "/static/media/heart.7c6c893d.svg"
+  },
+  {
+    "revision": "8365bbc6c007e1e8e3744b586351b721",
+    "url": "/static/media/image@2x.8365bbc6.jpg"
+  },
+  {
+    "revision": "5bad3fe9ed84355a3085a3864627c2eb",
+    "url": "/static/media/phone.5bad3fe9.svg"
+  },
+  {
+    "revision": "518eebec6b2b234861165d2b998a7321",
+    "url": "/static/media/pin.518eebec.svg"
+  },
+  {
+    "revision": "9a5d93ab08e07b55e107551ee70eec16",
+    "url": "/static/media/star.9a5d93ab.svg"
+  },
+  {
+    "revision": "1865d748e72c14627d90a97b76c5aac9",
+    "url": "/static/media/white-arrow.1865d748.svg"
+  }
+]);
